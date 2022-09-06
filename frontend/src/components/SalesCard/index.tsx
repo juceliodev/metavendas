6aa9d39baa119f570e36444fa11e13193e6d85d4
jucelio <juceliodesa@gmail.com>
2022-09-05 23:27:20 -0300

    useState

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+
+/**Importacao de biblioteca esterna para trabalhar com datas */
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -5,22 +8,30 @@ import NotificationButton from "../notificationButton";
 import "./style.css";
 
 function SalesCard() {
+
+    /**Macete para setar uma data de X dias atras */
+    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+
+    const [minDate , setMinDate] = useState(min);
+    const [maxDate, setMaxDate] = useState(new Date());
+
+
   return (
     <div className="dsmeta-card">
       <h2 className="dsmeta-sales-title">Vendas</h2>
       <div>
         <div className="dsmeta-form-control-container">
           <DatePicker
-            selected={new Date()}
-            onChange={(date: Date) => {}}
+            selected={minDate}
+            onChange={(date: Date) => setMinDate(date)}
             className="dsmeta-form-control"
             dateFormat="dd/MM/yyyy"
           />
         </div>
         <div className="dsmeta-form-control-container">
           <DatePicker
-            selected={new Date()}
-            onChange={(date: Date) => {}}
+            selected={maxDate}
+            onChange={(date: Date) => setMaxDate(date)}
             className="dsmeta-form-control"
             dateFormat="dd/MM/yyyy"
           />
