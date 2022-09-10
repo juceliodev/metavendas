c8d37fab5318f846ebe3e558e788105d59f0a79f
jucelio <juceliodesa@gmail.com>
2022-09-10 17:36:18 -0300

    Fix React corrigindo problema de sms ao fazer deploy frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+/**Essa linha de codigo é para corrigir um problema de envio de sms que estava dando ao fazer
+ * o deploy da aplicacao frontend no Netlify
+ */
+window.React = React
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
